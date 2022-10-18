fb8a8a2326303b1b35afb629f37beed5447e0209
Thanushh <thanush2112@gmail.com>
2022-10-18 17:01:33 +0530

    s

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello Everyone</h1>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+        </div>
+    );
 }
 
 export default App;
